791b08c2f7b0b68f6ff83a36797b7d6e5cfb06fd
MaratDavletshin <davletshinm@list.ru>
2025-01-06 21:26:52 +0100

    Create class GameService, and Instance Variables

--- src/main/java/com/tabled/millioner/controller/Controller.java
+++ src/main/java/com/tabled/millioner/controller/Controller.java
@@ -84,4 +84,190 @@ public class Controller {
         );
 
     }
+
+    @FXML
+    protected void onAButtonClick() throws NoSuchFieldException, IllegalAccessException {
+        String answer = gameService.processAnswer(a.getText(), randomIndex);
+        handleAnswerResult(answer);
+    }
+
+    @FXML
+    protected void onBButtonClick() throws NoSuchFieldException, IllegalAccessException {
+        String answer = gameService.processAnswer(b.getText(), randomIndex);
+        handleAnswerResult(answer);
+    }
+
+    @FXML
+    protected void onCButtonClick() throws NoSuchFieldException, IllegalAccessException {
+        String answer = gameService.processAnswer(c.getText(), randomIndex);
+        handleAnswerResult(answer);
+    }
+
+    @FXML
+    protected void onDButtonClick() throws NoSuchFieldException, IllegalAccessException {
+        String answer = gameService.processAnswer(d.getText(), randomIndex);
+        handleAnswerResult(answer);
+    }
+
+    @FXML
+    protected void onStartButtonClick() throws NoSuchFieldException, IllegalAccessException {
+        setQuestion();
+        setButtonsDisabled(false);
+        highlightLevel(gameService.getGameState().getCurrentLevel());
+
+    }
+
+    @FXML
+    protected void on50ButtonClick() {
+        // Проверяем, использована ли функция ранее
+        if (gameService.getGameState().isFiftyUsed()) {
+            showAlert("used", "used", "used");
+            System.out.println("50:50 used!");
+            return;
+        }
+
+        // Отмечаем, что 50:50 используется
+        gameService.getGameState().setFiftyUsed(true);
+
+        // Получаем правильный ответ для текущего вопроса
+        String correctAnswer = gameService.getQuestions().get(randomIndex).getCorrectAnswer();
+
+        // Собираем кнопки с ответами
+        List<Button> answerButtons = Arrays.asList(a, b, c, d);
+
+        // Находим неправильные кнопки
+        List<Button> wrongButtons = new ArrayList<>();
+        for (Button button : answerButtons) {
+            if (!button.getText().equals(correctAnswer)) {
+                wrongButtons.add(button);
+            }
+        }
+        // Удаляем две случайные неправильные кнопки
+        Collections.shuffle(wrongButtons);
+        for (int i = 0; i < 2; i++) {
+            wrongButtons.get(i).setDisable(true); // Делаем кнопку неактивной
+        }
+        System.out.println("50:50 применено!");
+    }
+
+
+    @FXML
+    protected void onJokerButtonClick() {
+        if (gameService.getGameState().isJokerUsed()) {
+            showAlert("used", "used", "Joker is already used");
+            System.out.println("used!");
+            return;
+        }
+        gameService.getGameState().setJokerUsed(true);
+        String correctAnswer = gameService.getQuestions().get(randomIndex).getCorrectAnswer();
+        a.setText(correctAnswer);
+        b.setText(correctAnswer);
+        c.setText(correctAnswer);
+        d.setText(correctAnswer);
+
+    }
+
+    @FXML
+    protected void onSpracheButtonClick() {
+        if (language.getText().equals("sprache - de")) {
+            language.setText("sprache - en");
+            gameService.getGameState().setLanguage("en");
+            gameService.setQuestions();
+        } else {
+            language.setText("sprache - de");
+            gameService.getGameState().setLanguage("de");
+            gameService.setQuestions();
+        }
+        System.out.println("Sprache button pushed: " + language.getText() + " set" );
+    }
+
+    private void setQuestion(){
+        System.out.println("gameService.getQuestions().size(): -> " + gameService.getQuestions().size());
+        int size = gameService.getQuestions().size();
+        randomIndex = random.nextInt(size);
+        System.out.println("random index: " + randomIndex);
+        // get random question
+        if (!gameService.getQuestions().isEmpty()) {
+            Question question = gameService.getQuestions().get(randomIndex);
+            updateUIWithQuestion(question);
+        } else {
+            question.setText("No questions available!");
+        }
+    }
+
+    private void updateUIWithQuestion(Question questionData) {
+        question.setText(questionData.getText());
+        a.setText(questionData.getAnswers().get(0));
+        b.setText(questionData.getAnswers().get(1));
+        c.setText(questionData.getAnswers().get(2));
+        d.setText(questionData.getAnswers().get(3));
+    }
+
+    private void handleAnswerResult(String result) throws NoSuchFieldException, IllegalAccessException {
+        switch (result) {
+            case "win":
+                // Показать экран победы
+                System.out.println("Congratulations, you won!");
+                // todo Make animation and sounds?
+                setButtonsDisabled(true);
+                highlightLevel(gameService.getGameState().getCurrentLevel());
+                showAlert("WIN", "WIN", "Congrats!!! you won 1kk€");
+                break;
+            case "lose":
+                System.out.println("Game over!");
+                // todo Make animation and sounds?
+                setButtonsDisabled(true);
+                showAlert("Lose", "Lose", "tut mir Leid, du hast надо тут сумму написать ");
+                break;
+            case "next":
+                System.out.println("Next question...");
+                highlightLevel(gameService.getGameState().getCurrentLevel());
+                // todo Make animation and sounds?
+                setQuestion();
+                enableAllButtons(false);
+                break;
+        }
+    }
+
+    private void setButtonsDisabled(boolean disabled) {
+        a.setDisable(disabled);
+        b.setDisable(disabled);
+        c.setDisable(disabled);
+        d.setDisable(disabled);
+        question.setDisable(disabled);
+        fifty.setDisable(disabled);
+        joker.setDisable(disabled);
+        // convert
+        start.setDisable(!disabled);
+        language.setDisable(!disabled);
+    }
+
+    private void enableAllButtons(boolean enabled) {
+        a.setDisable(enabled);
+        b.setDisable(enabled);
+        c.setDisable(enabled);
+        d.setDisable(enabled);
+        fifty.setDisable(gameService.getGameState().isFiftyUsed());
+        joker.setDisable(gameService.getGameState().isJokerUsed());
+    }
+
+    // css for level
+    public void highlightLevel(int level) throws NoSuchFieldException, IllegalAccessException {
+        // remove color
+        for (int i = 1; i <= 15; i++) {
+            Label label = (Label) getClass().getDeclaredField("label_" + i).get(this);
+            label.getStyleClass().remove("highlighted");
+        }
+        // add color
+        Label currentLabel = (Label) getClass().getDeclaredField("label_" + level).get(this);
+        currentLabel.getStyleClass().add("highlighted");
+    }
+
+    public void showAlert(String title, String textHeader, String text) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(textHeader);
+        alert.setContentText(text);
+        alert.showAndWait();
+    }
 }
