013424226fd777b0e7cb31900ba8363a26d95d01
Ek-eri <ismailekeri@gmail.com>
2025-01-06 20:12:15 +0100

    Initialize class Gamestate with instance Variables

--- src/main/java/com/tabled/millioner/models/GameState.java
+++ src/main/java/com/tabled/millioner/models/GameState.java
@@ -1,1 +1,9 @@
 package com.tabled.millioner.models;
+
+public class GameState {
+    private int currentLevel;
+    private boolean isFiftyUsed;
+    private boolean isJokerUsed;
+    private String language = "en";
+
+}
